ab075d3ddee6c2a77e28c208fb55222fca6c1f07
Nickolay Smirnov <Nickolay.Smirnov@hotmail.co.uk>
2023-10-08 19:16:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # jcbcheckinator
+
+Calculates the volume of cells and DMEM to mix
+
+# Usage
+Example:
+```
+python jcbccheckinator.py 18 2 6
+```
+This calculates volumes for 18 wells, with a live cell count of 2×10^6
---
+++ jcbcheckinator.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('wells', type=int, metavar='Number of wells')
+    parser.add_argument('cells', type=int, metavar='Live cell count')
+    parser.add_argument('power', nargs='?', default=0, type=int, help='Power of 10 to multiply the cell count by (default 0)', metavar='Cell power')
+    parser = parser.parse_args()
+    cellVolume = parser.wells * 50000000 * 10 ** -parser.power / parser.cells
+    print(f'Add {cellVolume} µl cells to {parser.wells * 80 - cellVolume} µl DMEM')
+
+
+if __name__ == '__main__':
+    main()
